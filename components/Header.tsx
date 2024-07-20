94c22999343907e2b1dbe79c70866605a565e5a0
Karan Patel <96780293+karanop001018@users.noreply.github.com>
2024-07-20 12:30:02 +0530

    First Commit

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <link href='/' className='md:flex-1'>
+        <Image
+            src="/assets/icons/logo.svg"
+            alt="Logo with name"
+            width={120}
+            height={32}
+            className="hidden md:block"
+        />
+        </link>
+    </div>
+  )
+}
+
+export default Header
